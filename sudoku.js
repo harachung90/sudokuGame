432169ab26bfcdf28464ec27ca33dccd249556cc
Hara Chung <harachung90@gmail.com>
2022-11-10 16:55:09 +1100

    Second commit. Created the board and tile.

---
+++ sudoku.js
@@ -0,0 +1,55 @@
+var numSelected = null;
+var tileSelected = null;
+
+var errors = 0;
+
+var board = [
+    "--74916-5",
+    "2---6-3-9",
+    "-----7-1-",
+    "-586----4",
+    "--3----9-",
+    "--62--187",
+    "9-4-7---2",
+    "67-83----",
+    "81--45---"
+]
+
+var solution = [
+    "387491625",
+    "241568379",
+    "569327418",
+    "758619234",
+    "123784596",
+    "496253187",
+    "934176852",
+    "675832941",
+    "812945763"
+]
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    // digits 1-9
+    for (let i = 1; i<=9; i++) {
+        //
+        let number = document.createElement("div");
+        number.id = i
+        number.innerText = i;
+        number.classList.add("number");
+        document.getElementById("digits").appendChild(number);
+    }
+
+    // board 9x9
+    for (let r= 1; r<=9; r++) {
+        for (let c = 0; c < 9; c++) {
+            let tile= document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            document.getElementById("board").appendChild(tile);
+        }
+
+    }
+}
