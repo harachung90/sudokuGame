e76540a02f615d98bd322a7a189ffdcbe5b7f645
Hara Chung <harachung90@gmail.com>
2022-11-11 20:32:04 +1100

    Third commit. Board is lined and now the numbers are ready to be selected

--- sudoku.js
+++ sudoku.js
@@ -27,29 +27,60 @@ var solution = [
     "812945763"
 ]
 
-window.onload = function() {
+window.onload = function () {
     setGame();
 }
 
 function setGame() {
     // digits 1-9
-    for (let i = 1; i<=9; i++) {
+    for (let i = 1; i <= 9; i++) {
         //
         let number = document.createElement("div");
         number.id = i
         number.innerText = i;
+        number.addEventListener("click", selectNumber);
         number.classList.add("number");
         document.getElementById("digits").appendChild(number);
     }
 
     // board 9x9
-    for (let r= 1; r<=9; r++) {
+    for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
-            let tile= document.createElement("div");
+            let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
+            if (board[r][c] != "-") {
+                tile.innerText = board[r][c];
+                tile.classList.add("tile-start");
+            }
+            if (r == 2 || r == 5) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 2 || c == 5) {
+                tile.classList.add("vertical-line");
+            }
+
+            tile.addEventListener("click", selectTile);
             tile.classList.add("tile");
             document.getElementById("board").appendChild(tile);
         }
 
+    }
+}
+
+function selectNumber() {
+    if (numSelected != null) {
+        numSelected.classList.remove("number-selected");
+    }
+    numSelected = this;
+    numSelected.classList.add("number-selected");
+
+}
+
+function selectTile() {
+    if (numSelected) {
+        if (this.innerText != "") {
+            return;
+        }
+        this.innerText = numSelected.id;
     }
 }
