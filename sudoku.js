5e14758ebe6c582e1eeca14794655ed6f5ebe895
Hara Chung <harachung90@gmail.com>
2022-11-12 15:21:08 +1100

    Fourth and final commit. Sudoku game complete

--- sudoku.js
+++ sudoku.js
@@ -81,6 +81,17 @@ function selectTile() {
         if (this.innerText != "") {
             return;
         }
-        this.innerText = numSelected.id;
+        // "0-0" "0-1" ... "3-1"
+        let coords = this.id.split("-"); // ["0","0"]
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+
+        if (solution[r][c] == numSelected.id) {
+            this.innerText = numSelected.id;
+        } else {
+            errors += 1;
+            document.getElementById("errors").innerText = errors;
+        }
+
     }
 }
